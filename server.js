d6d246d3aa2df1ecea440504894d20b2abf7a44d
Abdalrhman <alattarabdalrhman@gmail.com>
2025-05-14 14:14:32 +0300

    organize code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -10,17 +9,10 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-
-const PORT = process.env.PORT || 3002;
-
 app.use("/api/products", productsRoutes);
 app.use("/", homeRoutes);
 app.use(notFoundRoutes);
 
+const PORT = process.env.PORT || 3002;
 
-
-
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-
-});
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
